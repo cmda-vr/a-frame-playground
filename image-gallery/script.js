83dd6c0253193c989dd208a6f37cd300cda486d6
dandevri <hi@dandevri.es>
2018-02-20 16:53:51 +0100

    Add image gallery

---
+++ image-gallery/script.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function() {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function() {
+      // fade out image
+      data.target.emit('set-image-fade');
+      // wait for fade to complete
+      setTimeout(function() {
+        data.target.setAttribute('material', 'src', data.src);
+      }, data.dur);
+    })
+  },
+
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+})
+
+console.log('stuff');
